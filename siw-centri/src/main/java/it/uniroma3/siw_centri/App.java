e7cad07a7fb701a2db169e8127b3374cfe64429b
Kaio Cesar Alves Reis <kaiocesar.alvesreis@gmail.com>
2018-05-29 16:59:26 +0200

    Aggiornamento

---
+++ siw-centri/src/main/java/it/uniroma3/siw_centri/App.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw_centri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
